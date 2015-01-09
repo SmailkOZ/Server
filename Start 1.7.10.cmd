70faf0f377f3536ba78d6b9a67b676a135e006fb
Cufee <wuwq@list.ru>
2015-01-09 17:51:23 +0200

    Сделал все! Полностью рабочая сборка!
    Сделай репо для Клиента

--- Start 1.7.10.cmd
+++ Start 1.7.10.cmd
@@ -1,4 +1,4 @@
 @ECHO OFF
  SET BINDIR=%~dp0
  CD /D "%BINDIR%"
- java -Xms128M -Dfile.encoding=UTF-8 -jar cauldron-1.7.10-1.1240.06.213-server.jar
+ java -Xms4G -Dfile.encoding=UTF-8 -jar cauldron-1.7.10-1.1240.06.213-server.jar
