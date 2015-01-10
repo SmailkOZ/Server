997b1171b583cda64783be7ab05db7b1fef5c37f
Cufee <wuwq@list.ru>
2015-01-10 16:10:38 +0200

    Закончил сборку. Думаю есть недочеты.
    Все=)

--- Start 1.7.10.cmd
+++ Start 1.7.10.cmd
@@ -1,4 +1,4 @@
 @ECHO OFF
  SET BINDIR=%~dp0
  CD /D "%BINDIR%"
- java -Xms4G -Dfile.encoding=UTF-8 -jar cauldron-1.7.10-1.1240.06.213-server.jar
+ java -Xms2G -Dfile.encoding=UTF-8 -jar cauldron-1.7.10-1.1240.06.213-server.jar -Xmx12G
